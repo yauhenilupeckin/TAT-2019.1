0579f102500446f5231ac50da2cb6110dc2830ab
yauheniLupekin <48407219+yauheniLupekin@users.noreply.github.com>
2019-03-22 17:17:32 +0300

    Add files via upload

---
+++ Jun.cs
@@ -0,0 +1,15 @@
+﻿namespace DEV_3
+{
+    class Junior : Employee
+    {
+        public Junior()
+        {
+            Cost = 10;
+            Productivity = 5;
+        }
+        public override string GetName()
+        {
+            return "Junior";
+        }
+    }
+}
