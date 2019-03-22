31e5eebbfb7fffd8afaaf219d0391e27423c64d4
yauheniLupekin <48407219+yauheniLupekin@users.noreply.github.com>
2019-03-22 17:20:54 +0300

    Add files via upload

---
+++ Middle.cs
@@ -0,0 +1,15 @@
+﻿namespace DEV_3
+{
+    class Middle : Junior
+    {
+        public Middle()
+        {
+            Cost += 5;
+            Productivity *= 3;
+        }
+        public override string GetName()
+        {
+            return "Middle";
+        }
+    }
+}
